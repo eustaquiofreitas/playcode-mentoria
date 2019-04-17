127f157121e466013203a62c35ceebd5b62c913f
eustaquiofreitas <eustaquio.efb@gmail.com>
2019-04-17 14:26:39 -0300

    projetos

---
+++ proj011/src/programa1.js
@@ -0,0 +1,38 @@
+/* 
+
+## Referências:
+
+https://www.youtube.com/watch?v=Ri20j59UFYw
+https://www.youtube.com/watch?v=-QWGj1D636w
+
+*/
+
+/******************************************************************************* 
+	Tarefa 1: 
+		Atribuir o número 8 a uma variável chamada tomates
+*******************************************************************************/
+let tomates = 8;
+
+
+/******************************************************************************* 
+	Tarefa 2: 
+		Atribuir o número quatro e meio, a uma variável chamada melancias 
+*******************************************************************************/
+let melancias = 4.5; 
+
+
+/******************************************************************************* 
+  Tarefa 3: 
+  	Atribuir o texto Bom dia à uma variável chamada mensagem 
+*******************************************************************************/
+let mensagem ="Bom dia";
+
+
+/******************************************************************************* 
+	Tarefa 4: 
+    Atribuir o valor booleano verdadeiro à uma variável chamada ligado e o valor
+    booleano falso para uma variável chamada desligado.
+*******************************************************************************/
+
+let ligado = true;
+let desligado = false;
---
+++ proj012/src/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+function triplicarValor(){
+	let numero = prompt("Digite um número: ");
+		return resultado = numero * 3;
+
+}
