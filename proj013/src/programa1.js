29b9e7214ce24e976d784cd9e3088db79358c40b
eustaquiofreitas <eustaquio.efb@gmail.com>
2019-04-22 11:35:04 -0300

    Projetos-mentoria

--- proj013/src/programa1.js
+++ proj013/src/programa1.js
@@ -5,16 +5,13 @@
 		a mensagem "Temperatura negativa". Se for maior que zero, retorna a mensagem
 		"Temperatura positiva"
 *******************************************************************************/
-function termostato(temperatura){
-	if(temperatura < 0){
+function termostato(temperatura) {
+	if (temperatura < 0) {
 		return "Temperatura negativa";
-	}
-	else if(temperatura > 0){
+	}else {
 		return "Temperatura positiva";
 	}
 }
-console.log(termostato(10));
-
 /*******************************************************************************
 	Tarefa 2:
 		Crie uma função chamada boletimEscolar. Essa função deve receber um número
@@ -23,47 +20,70 @@ console.log(termostato(10));
 		nota válida, a função deve retornar "Aprovado" ou "Reprovado". A média para
 		aprovação é 60%.
 *******************************************************************************/
-function boletimEscolar(){
-	
+function boletimEscolar(nota){
+	if(nota < 0 || nota > 10){
+		console.log("Nota inválida")	;
+	}else{
+		if (nota < 6){
+			console.log("Aluno REPROVADO");
+		} else {
+			console.log("Aluno APROVADO");
+		}
+	}
 }
-
-
-
-/*******************************************************************************
-	Tarefa 3:
-		Crie uma função chamada calcularFrete que recebe a sigla do estado como
-		parâmetro e retorne o valor do frete. O frete deve ser calculado de acordo
-		com a seguinte tabela de preços:
-		- MG     -> 12,50
-		- SP     -> 23,90
-		- RJ     -> 18,20
-		- Outros -> 35,90
-*******************************************************************************/
-
-
-
-
-
+boletimEscolar(-2);
 
 
 /*******************************************************************************
-	Tarefa 4:
-		Crie uma função com o nome calcularAltura. Essa função deve receber um
-		parâmetro altura e retornar uma mensagem de acordo com a tabela:
-		- menos que 1,60 -> "baixinho"
-		- de 1,60 a 1,85 -> "altura normal"
-		- mais de um 1,85 -> "joga basquete?"
-*******************************************************************************/
+// 	Tarefa 3:
+// 		Crie uma função chamada calcularFrete que recebe a sigla do estado como
+// 		parâmetro e retorne o valor do frete. O frete deve ser calculado de acordo
+// 		com a seguinte tabela de preços:
+// 		- MG     -> 12,50
+// 		- SP     -> 23,90
+// 		- RJ     -> 18,20
+// 		- Outros -> 35,90
+// *******************************************************************************/
+var estados = {MG: "12,50",SP: "23,90",RJ: "18,20",Outros: "35,90"}
+function calcularFrete(estado){
+	if(estado == "MG"){
+		console.log(estados.MG);
+	}
+}
+calcularFrete("MG");
 
-
-
-
-
-
-
-/*******************************************************************************
-	Tarefa 5:
-		Crie uma função chamada validarSenha. Se a senha for "123456", a função
-		retorna a mensagem "Login efetuado com sucesso", senão, retorna a mensagem
-		"Senha inválida".
-*******************************************************************************/
+// /*******************************************************************************
+// 	Tarefa 4:
+// 		Crie uma função com o nome calcularAltura. Essa função deve receber um
+// 		parâmetro altura e retornar uma mensagem de acordo com a tabela:
+// 		- menos que 1,60 -> "baixinho"
+// 		- de 1,60 a 1,85 -> "altura normal"
+// 		- mais de um 1,85 -> "joga basquete?"
+// *******************************************************************************/
+function calcularAltura(altura){
+	if(altura < 1.6){
+		return "baixinho";
+	}
+	else if(altura >= 1.60 & altura <= 1.85){
+		return "altura normal";
+	}
+	else{
+		return "joga basquete?";
+	}
+}
+calcularAltura(1.9)
+// /*******************************************************************************
+// 	Tarefa 5:
+// 		Crie uma função chamada validarSenha. Se a senha for "123456", a função
+// 		retorna a mensagem "Login efetuado com sucesso", senão, retorna a mensagem
+// 		"Senha inválida".
+// *******************************************************************************/
+function validarSenha(senha){
+	if(senha === "123456"){
+		return "Login efetuado com sucesso";
+	}
+	else{
+		return "Senha inválida";
+	}
+}
+validarSenha(123456);
--- proj014/src/programa1.js
+++ proj014/src/programa1.js
@@ -1,39 +1,122 @@
 /*******************************************************************************
-	Tarefa 1: (USE SWITCH CASE) 
-		Crie uma função chamada calcularFrete que recebe a sigla do estado como 
-		parâmetro e retorne o valor do frete. O frete deve ser calculado de acordo 
-		com a seguinte tabela de preços:
+	Tarefa 1: (USE SWITCH CASE)
+		Crie uma função chamada calcularFrete que recebe a sigla do estado como parâmetro e retorne o valor do frete. O frete deve ser calculado de acordo com a seguinte tabela de preços:
 			MG     -> 12,50
 			SP     -> 23,90
 			RJ     -> 18,20
 			Outros -> 35,90
 *******************************************************************************/
+function calcularFrete(estado) {
 
+	switch(estado){
+		case "MG":
+			return "O valor do frete para Minas Gerais é 12,50";
+			break;
+		case "SP":
+			return "O valor do frete para São Paulo é 23,90";
+			break;
+		case "RJ":
+			return "O valor do frete para Rio de Janeiro é 18,20";
+			break;
+		default:
+			return "O valor do frete para outros estados é 35,90";
+	}
 
-
-
-
+}
+calcularFrete("MG");
 
 
 /*******************************************************************************
 	Tarefa 2: (USE SWITCH CASE)
-		Crie um função chamada nomeMes, que recebe um número referente ao mês e 
-		retorna o nome do mês equivalente. 1 para Janeiro, 2 para Fevereiro, 3 para 
+		Crie um função chamada nomeMes, que recebe um número referente ao mês e
+		retorna o nome do mês equivalente. 1 para Janeiro, 2 para Fevereiro, 3 para
 		março e assim por diante, até 12 para dezembro. Deve retornar a mensagem
 		"Mês inválido" caso seja usado algum número que não esteja entre 1 e 12.
 *******************************************************************************/
 
+function nomeMes(mes) {
+	switch (mes) {
+		case 1:
+			return "Janeiro";
+			break;
+		case 2:
+			return "Fevereiro";
+			break;
+		case 3:
+			return "Março";
+			break;
+		case 4:
+			return "Abril";
+			break;
+		case 5:
+			return "Maio";
+			break;
+		case 6:
+			return "Junho";
+			break;
+		case 7:
+			return "Julho";
+			break;
+		case 8:
+			return "Agosto";
+			break;
+		case 9:
+			return "Setembro";
+			break;
+		case 10:
+			return "Outubro";
+			break;
+		case 11:
+			return "Novembro";
+			break;
+		case 12:
+			return "Dezembro";
+			break;
+		default:
+			return "Mês inválido";
+	}
+}
+nomeMes(1);
 
-
-
-
-
-
-/*******************************************************************************
-	Tarefa 3: (USE SWITCH CASE) 
+/***************************
+	Tarefa 3: (USE SWITCH CASE)
 		Crie uma função chamada tabelaPeriodica que recebe como parâmetro a sigla
-		de um elemento químico e retorne o nome desse elemento. Para exemplificar, 
+		de um elemento químico e retorne o nome desse elemento. Para exemplificar,
 		passando a sigla Zn, vai retornar zinco e Pt vai retornar platina.
 		Use essa referência para abranger todos os elementos:
-		https://pt.wikipedia.org/wiki/Tabela_peri%C3%B3dica#Estrutura_da_tabela_peri%C3%B3dica		
+		https://pt.wikipedia.org/wiki/Tabela_peri%C3%B3dica#Estrutura_da_tabela_peri%C3%B3dica
 *******************************************************************************/
+
+function tabelaPeriodica(elemento){
+	switch(elemento){
+		case "Rh":
+		return "Ródio";
+		break;
+
+		case "Si":
+		return "Sílicio";
+		break;
+
+		case "Ba":
+		return "Bário";
+		break;
+
+		case "Pb":
+		return "Chumbo";
+		break;
+
+		case "P":
+		return "Fósforo";
+		break;
+
+		case "Mg":
+		return "Magnésio"
+		break;
+
+		case "Fr":
+		return "Frâncio";
+		break;
+	}
+}
+
+tabelaPeriodica("Mg");
